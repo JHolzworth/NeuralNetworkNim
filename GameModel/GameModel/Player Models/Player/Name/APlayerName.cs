223270c9fd917eee79fbf3e5d38bbf6d03cf580a
JHolzworth <jholzworth@student.neumont.edu>
2013-09-02 02:51:03 -0600

    Started working on the player models.

---
+++ GameModel/GameModel/Player Models/Player/Name/APlayerName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameModel.Player_Models.Player.Name
+{
+    abstract class APlayerName : IPlayerName
+    {
+    }
+}
